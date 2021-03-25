900b2ec34a70852ef54c3d339e216632ed929454
ThatOneCalculator <kainoakanter@gmail.com>
2021-03-25 11:50:59 -0700

    the stuff

--- scr/search.js
+++ scr/search.js
@@ -3,17 +3,15 @@ window.onload = function() {
 }
 
 function initSearchBar() {
-    const searchBarInput = document.getElementById("search-bar-input")
-    
-    searchBarInput.value = ""
-    searchBarInput.focus()
+    document.getElementById("search-bar-input").value = ""
+    document.getElementById("search-bar-input").focus()
 
-    searchBarInput.addEventListener("keypress", (event) => {
-        if (event.key !== 'Enter') return
+    document.getElementById("search-bar-input").addEventListener("keypress", (event) => {
+        if (event.key != 'Enter') return
 
-        const searchEnginer = "https://www.duckduckgo.com/?q="
-        const endQuery = "&atb=v225-7&ia=web"
-       
-        document.location = searchEnginer + searchBarInput.value.replace(/\ /g, "+") + endQuery
+        googleSearchUrl = "https://www.searx.bar/?q="
+        //otherThing = "&atb=v225-7&ia=web"
+        query = document.getElementById("search-bar-input").value.replace(/\ /g, "+")
+        document.location = googleSearchUrl + query
     })
 }
